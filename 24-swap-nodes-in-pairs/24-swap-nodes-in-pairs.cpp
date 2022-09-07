7e90957fc5c8a3e0fc2b1dca4589a5ab4c111284
Abhishek kumar tiwari <56756619+Pubsch123@users.noreply.github.com>
2022-09-07 23:18:51 +0530

    Time: 6 ms (32.69%), Space: 7.6 MB (54.96%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* nptr = head->next;
+        head->next = swapPairs(nptr->next);
+        nptr->next = head;
+        return nptr;
+    }
+};
